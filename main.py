ab40cbe1a51c35891695b1c84bf7625b8fd7e252
Gabangxa <63610188+Gabangxa@users.noreply.github.com>
2025-03-19 19:22:13 +0000

    Restored to '690d971beb7b13847a6936a545e0d956bee17739'
    Replit-Restored-To: 690d971beb7b13847a6936a545e0d956bee17739

--- main.py
+++ main.py
@@ -11,13 +11,5 @@ else:
     logger = logging.getLogger(__name__)
     logger.info("Development server started")
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/faq')
-def faq():
-    return render_template('faq.html')
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
